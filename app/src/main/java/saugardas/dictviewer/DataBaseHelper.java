fc47a9a5df612059a7784cbe28e59bb0ae84cdb7
Василий Петров <saugardas@yandex.ru>
2016-08-28 23:23:47 +0300

    read own database

---
+++ app/src/main/java/saugardas/dictviewer/DataBaseHelper.java
@@ -0,0 +1,101 @@
+package saugardas.dictviewer;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private static String DB_PATH = "/data/data/saugardas.dictviewer/databases/";
+    private static String DB_NAME = "dict.sqlite";
+    private SQLiteDatabase myDataBase;
+    private final Context mContext;
+
+    public DataBaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+        this.mContext = context;
+    }
+
+    // Создает пустую базу данных и перезаписывает её нашей собственной базой
+    public void createDataBase() throws IOException {
+        boolean dbExist = checkDataBase();
+        if (!dbExist) {
+            //вызывая этот метод создаем пустую базу, позже она будет перезаписана
+            this.getReadableDatabase();
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+    }
+
+    // Проверяет, существует ли уже эта база, чтобы не копировать каждый раз при запуске приложения
+    private boolean checkDataBase() {
+        SQLiteDatabase checkDB = null;
+        try {
+            String myPath = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        } catch(SQLiteException e) {
+            // база еще не существует
+        }
+        if(checkDB != null){
+            checkDB.close();
+        }
+        return checkDB != null;
+    }
+
+    // Копирует базу из папки assets заместо созданной локальной БД
+    // Выполняется путем копирования потока байтов.
+    private void copyDataBase() throws IOException {
+        //Открываем локальную БД как входящий поток
+        InputStream myInput = mContext.getAssets().open(DB_NAME);
+
+        //Путь ко вновь созданной БД
+        String outFileName = DB_PATH + DB_NAME;
+
+        //Открываем пустую базу данных как исходящий поток
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //перемещаем байты из входящего файла в исходящий
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0){
+            myOutput.write(buffer, 0, length);
+        }
+
+        //закрываем потоки
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    public void openDataBase() throws SQLException {
+        //открываем БД
+        String myPath = DB_PATH + DB_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    @Override
+    public synchronized void close() {
+        if(myDataBase != null)
+            myDataBase.close();
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+}
--- app/src/main/java/saugardas/dictviewer/MainActivity.java
+++ app/src/main/java/saugardas/dictviewer/MainActivity.java
@@ -1,7 +1,15 @@
 package saugardas.dictviewer;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DataBaseHelper myDbHelper = new DataBaseHelper(this);
+
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+
+        try {
+            myDbHelper.openDataBase();
+        } catch (SQLException sqle) {
+            throw sqle;
+        }
+
+        // создаем объект для данных
+        ContentValues cv = new ContentValues();
+        // подключаемся к БД
+        SQLiteDatabase db = myDbHelper.getWritableDatabase();
+
+        Cursor c = db.query("dictionary", null, null, null, null, null, null);
+
+        // определяем номера столбцов по имени в выборке
+
+        // ставим позицию курсора на первую строку выборки
+        // если в выборке нет строк, вернется false
+        if (c.moveToFirst()) {
+            // определяем номера столбцов по имени в выборке
+            int idColIndex = c.getColumnIndex("_id");
+            int wordColIndex = c.getColumnIndex("word");
+            do {
+                // получаем значения по номерам столбцов и пишем все в лог
+                Log.d("LOG",
+                        "ID = " + c.getInt(idColIndex) + ", word = "
+                                + c.getString(wordColIndex));
+                // переход на следующую строку
+                // а если следующей нет (текущая - последняя), то false -
+                // выходим из цикла
+            } while (c.moveToNext());
+
+            myDbHelper.close();
+        }
     }
 }
